8553d74c8235c6ae97293673d37dd7faf896ac75
Wesley Wesley <43004728+wesley08@users.noreply.github.com>
2022-03-08 11:16:08 +0000

    timestamp

--- myApp.js
+++ myApp.js
@@ -1,39 +1,57 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next();
+});
 
 
+// app.get('/now', function(req, res, next) {
+//   req.time =new Date().toString();  // Hypothetical synchronous operation
+//   next();
+// }, function(req, res) {
+//   res.json({time:req.time});
+// });
+
+// app.use("/public", express.static(__dirname + "/public"));
+// app.get('/',(req, res) => {
+// 		  res.sendFile(process.cwd() + '/views/index.html');
+//     });
 
 
+// app.get('/json',(req, res) => {
+//   if(process.env.MESSAGE_STYLE === "uppercase") return res.json({"message": "HELLO JSON"});
+//   return res.json({"message": "Hello json"});
+//     });
 
+// app.get('/:word/echo',  function(req, res) {
+//   res.json({echo:req.params.word});
+// });
 
+// app.get('/name',  function(req, res) {
+//   const {first,last}= req.query;
+//   res.json( { name: `${first} ${last}`});
+// });
 
+// app.post('/name',  function(req, res) {
+//   const {first,last}= req.body;
+//   res.json( { name: `${first} ${last}`});
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/api/:date*?',  function(req, res) {
+  const {date} = req.params;
+  const parseDate =  isNaN(date) ? date :parseInt(date);
+  const newDate = date ? new Date(parseDate) : new Date();
+  const utc = newDate.toUTCString();
+  const unix =  new Date(newDate).valueOf();
+  if(unix != "Invalid Date")
+    return res.json({unix,utc});
+  return res.json({ error : "Invalid Date" });
+});
 
  module.exports = app;
